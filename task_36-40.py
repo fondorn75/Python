b46a0cc76f68d43db53469b8c2879cc42969a0f6
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-02-01 11:33:11 +0300

    add task 36, 37

---
+++ task_36-40.py
@@ -0,0 +1,36 @@
+# 16. Задать список из n чисел последовательности (1+1/n)^n и вывести на экран их сумму
+
+def SummaNumberN(n):
+    tmp = []
+    for n in range(1, n + 1):
+        tmp.append((1 + 1 / n)**n)
+    return sum(tmp)
+
+
+print(SummaNumberN(10))
+
+# 17. Задать список из N элементов, заполненных числами из [-N, N]. Найти произведение элементов на указанных позициях.
+#     Позиции хранятся в файле file.txt в одной строке одно число
+
+
+def ListElements(N):
+    ListNumbers = []
+    for i in range(-N, N + 1):
+        ListNumbers.append(i)
+
+    data = open("file.txt", "r")
+    for line in data:
+        print(ListNumbers[int(line)])
+    data.close()
+    return ListNumbers
+
+
+print(ListElements(5))
+
+# 18. Реализовать алгоритм перемешивания списка.
+
+
+# 19. Реализовать алгоритм задания случайных чисел. Без использования встроенного генератора псевдослучайных чисел
+
+
+# 20. Определить, присутствует ли в заданном списке строк, некоторое число
