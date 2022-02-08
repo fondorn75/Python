7a7e62780dbdd7481f6d5ab6ff49de02a5ef1946
fondorn75 <zatoichi.cadzicu@gmail.com>
2022-02-08 17:27:32 +0300

    add task 46

--- task_46-50.py
+++ task_46-50.py
@@ -1,8 +1,25 @@
 import math
 # 26. Дано число. Составить список чисел Фибоначчи, в том числе для отрицательных индексов
-# для положительных формула: F(n) = F(n-1) + F(n-1)
+# для положительных формула: F(n) = F(n-1) + F(n-2)
 # для отрицательных формула: F(n) = F(n+2) - F(n+1)
 
+def Fibonachi(n):
+    sum = 0
+    ch1 = 1
+    ch2 = 1
+    lstResult = []
+
+    for i in range(n):
+        if i < n:
+            sum = ch1 + ch2
+            ch1 = ch2
+            ch2 = sum
+            i += 1
+        lstResult.append(sum)
+
+    return lstResult
+
+print(Fibonachi(7))
 
 # 27. Строка содержит набор чисел. Показать большее и меньшее число
 
